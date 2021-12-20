733ddaa8b59f24347bd044f4bc3e0079963082bb
Zalouma <ahmedshahd350@gmail.com>
2021-12-21 01:10:06 +0200

    completeing Login register pages

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import Home from './pages/Home'
 import './App.css'
 import ProductList from './pages/ProductList'
 import Product from './pages/Product'
+import Register from './pages/Register'
 function App() {
     return (
         <div className="App">
-            <Product />
+            <Register />
         </div>
     )
 }
