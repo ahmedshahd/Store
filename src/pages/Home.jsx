f4ac738a01ad1dd7d6fce768855599d6e4ddd634
Zalouma <ahmedshahd350@gmail.com>
2021-12-20 02:58:51 +0200

    Adding FOOTER component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Announcement from '../components/Anouncement'
 import Categories from '../components/Catagories'
+import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
 import Newsletter from '../components/Newsletter'
 import Products from '../components/Products'
@@ -8,12 +9,13 @@ import Slider from '../components/Slider'
 const Home = () => {
     return (
         <div>
-            <Announcement />
             <NavBar />
+            <Announcement />
             <Slider />
             <Categories />
             <Products />
             <Newsletter />
+            <Footer />
         </div>
     )
 }
