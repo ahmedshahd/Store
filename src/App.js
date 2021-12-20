496c103ba9eebe75035e86b01772cb5222e8433f
Zalouma <ahmedshahd350@gmail.com>
2021-12-20 03:34:54 +0200

    completeing single product page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Home from './pages/Home'
 import './App.css'
 import ProductList from './pages/ProductList'
+import Product from './pages/Product'
 function App() {
     return (
         <div className="App">
-            <ProductList />
+            <Product />
         </div>
     )
 }
