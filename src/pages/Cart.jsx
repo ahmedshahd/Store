9266a4f2029be29083bfda4b3c1965b2fba247b1
Zalouma <ahmedshahd350@gmail.com>
2021-12-21 02:16:38 +0200

    MAKING CART RESPONSIVE

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,11 +3,13 @@ import styled from 'styled-components'
 import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
+import { mobile } from '../responsive'
 
 const Container = styled.div``
 
 const Wrapper = styled.div`
     padding: 20px;
+    ${mobile({ padding: '10px' })}
 `
 
 const Title = styled.h1`
@@ -27,12 +29,14 @@ const TopButton = styled.button`
     font-weight: 600;
     cursor: pointer;
     border: ${({ type }) => type === 'filled' && 'none'};
-    background-color: ${(type) =>
-        type === 'filled' ? 'black' : 'transparent'};
+    background-color: ${(props) =>
+        props.type === 'filled' ? 'black' : 'transparent'};
     color: ${({ type }) => type === 'filled' && 'white'};
 `
 
-const TopTexts = styled.div``
+const TopTexts = styled.div`
+    ${mobile({ display: 'none' })}
+`
 const TopText = styled.span`
     text-decoration: underline;
     cursor: pointer;
@@ -42,6 +46,7 @@ const TopText = styled.span`
 const Bottom = styled.div`
     display: flex;
     justify-content: space-between;
+    ${mobile({ flexDirection: 'column' })}
 `
 
 const Info = styled.div`
@@ -51,6 +56,7 @@ const Info = styled.div`
 const Product = styled.div`
     display: flex;
     justify-content: space-between;
+    ${mobile({ flexDirection: 'column' })}
 `
 
 const ProductDetail = styled.div`
@@ -77,7 +83,7 @@ const ProductColor = styled.div`
     width: 20px;
     height: 20px;
     border-radius: 50%;
-    background-color: ${(props) => props.color};
+    background-color: ${({ color }) => color};
 `
 
 const ProductSize = styled.span``
@@ -99,11 +105,13 @@ const ProductAmountContainer = styled.div`
 const ProductAmount = styled.div`
     font-size: 24px;
     margin: 5px;
+    ${mobile({ margin: '5px 15px' })}
 `
 
 const ProductPrice = styled.div`
     font-size: 30px;
     font-weight: 200;
+    ${mobile({ marginBottom: '20px' })}
 `
 
 const Hr = styled.hr`
@@ -128,8 +136,8 @@ const SummaryItem = styled.div`
     margin: 30px 0px;
     display: flex;
     justify-content: space-between;
-    font-weight: ${(props) => props.type === 'total' && '500'};
-    font-size: ${(props) => props.type === 'total' && '24px'};
+    font-weight: ${({ type }) => type === 'total' && '500'};
+    font-size: ${({ type }) => type === 'total' && '24px'};
 `
 
 const SummaryItemText = styled.span``
