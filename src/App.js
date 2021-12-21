d47d625bbc8d0e6fdaf53cf75ed1ad8e02a0ee75
Zalouma <ahmedshahd350@gmail.com>
2021-12-21 02:31:45 +0200

    MAKING Register and login pages  RESPONSIVE

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
     return (
         <div className="App">
             {/* <Cart /> */}
-            <Home />
+            {/* <Home /> */}
+            <Register />
         </div>
     )
 }
