6d7441ab407735ef9010ca6910b80b2d77f6693f
Zalouma <ahmedshahd350@gmail.com>
2021-12-21 02:38:52 +0200

    MAKING Product list page  RESPONSIVE

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
             {/* <Cart /> */}
             {/* <Home /> */}
             {/* <Register /> */}
-            <Product />
+            <ProductList />
         </div>
     )
 }
