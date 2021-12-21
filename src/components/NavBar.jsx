b73a6f3e2110d8e7a64b3f7fabc1e2675cd918dc
Zalouma <ahmedshahd350@gmail.com>
2021-12-21 02:21:02 +0200

    MAKING NavBar  RESPONSIVE

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,18 @@
 import styled from 'styled-components'
 import { Search, ShoppingCartOutlined } from '@material-ui/icons'
 import { Badge } from '@material-ui/core'
+import { mobile } from '../responsive'
+
 const Container = styled.div`
     height: 60px;
-    background-color: white;
+    ${mobile({ height: '50px' })}
 `
 const Wraper = styled.div`
     padding: 10px 20px;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    ${mobile({ padding: '10px 0px' })}
 `
 const Left = styled.div`
     display: flex;
@@ -23,6 +26,7 @@ const Right = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
+    ${mobile({ flex: 2, justifyContent: 'center' })}
 `
 
 const Center = styled.div`
@@ -39,16 +43,23 @@ const SearchContainer = styled.div`
 const Input = styled.input`
     border: none;
     outline: none;
+    ${mobile({ width: '50px' })}
 `
 const Logo = styled.h2`
     font-weight: bold;
+    ${mobile({ fontSize: '24px' })}
 `
 const MenuItems = styled.div`
     font-size: 14px;
     cursor: pointer;
     margin-left: 25px;
+    ${mobile({ fontSize: '12px', marginLeft: '10px' })}
 `
-const Language = styled.span``
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+    ${mobile({ display: 'none' })}
+`
 const NavBar = () => {
     return (
         <Container>
