862df343bdf4453d0c7b84f7b999efadc5dbe0ba
Zalouma <ahmedshahd350@gmail.com>
2021-12-21 02:07:42 +0200

    adding cart page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css'
 import ProductList from './pages/ProductList'
 import Product from './pages/Product'
 import Register from './pages/Register'
+import Cart from './pages/Cart'
 function App() {
     return (
         <div className="App">
-            <Register />
+            <Cart />
         </div>
     )
 }
